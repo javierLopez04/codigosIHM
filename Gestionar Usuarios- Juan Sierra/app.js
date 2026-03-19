fd9e81e14f7509dbc143efd60f705cd36558f59d
sierra495 <juasierra224@umariana.edu.co>
2026-03-19 00:19:53 -0500

    Add files via upload

---
+++ Gestionar Usuarios- Juan Sierra/app.js
@@ -0,0 +1,24 @@
+function ver() {
+    alert("Ver usuario");
+}
+
+function editar() {
+    alert("Editar usuario");
+}
+
+function eliminar() {
+    alert("Eliminar usuario");
+}
+
+function reporte() {
+    alert("Generando reporte...");
+}
+
+function pdf() {
+    alert("Exportando a PDF...");
+}
+
+function excel() {
+    alert("Exportando a Excel...");
+}
+    
