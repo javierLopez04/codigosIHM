c94a70cfb7839e46b702f544dce9e65caa219e45
sierra495 <juasierra224@umariana.edu.co>
2026-03-19 00:19:25 -0500

    Add files via upload

---
+++ Reporte Inventario-Juan Sierra/app.js
@@ -0,0 +1,15 @@
+function reporte() {
+    alert("Generando reporte...");
+}
+
+function pdf() {
+    alert("Exportando a PDF...");
+}
+
+function excel() {
+    alert("Exportando a Excel...");
+}
+
+function fecha() {
+    alert("Seleccionar fecha");
+}
